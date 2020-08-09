16bda0234f40c5c05af25921343ba2c79c44fb83
ninecolors <76292655@qq.com>
2020-08-09 18:28:28 +0800

    路由命名

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@
 */
 
 
-Route::get('/', 'StaticPagesController@home');
+Route::get('/', 'StaticPagesController@home')->name('home');
 
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/help', 'StaticPagesController@help')->name('help');
 
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
 
