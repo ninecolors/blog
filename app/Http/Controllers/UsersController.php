735efa277d2a4a40aa5c9ddd0db616e82358190a
ninecolors <76292655@qq.com>
2020-08-10 13:44:59 +0800

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,6 +12,14 @@ class UsersController extends Controller
         return view('users.create');
     }
 
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+    }
+
     public function show(User $user){
         return view('users.show',compact('user'));
     }
