19118efc43223448e57b031ad3c61223c2e788bf
ninecolors <76292655@qq.com>
2020-08-11 09:32:26 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,14 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth',[
+            'except' => ['create','show','store']
+        ]);
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
 
     public function create(){
         return view('users.create');
@@ -41,6 +49,7 @@ class UsersController extends Controller
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update',$user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
